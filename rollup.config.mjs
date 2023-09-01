c294b9827ab96a46b67118d9f5d32b7f396b66e7
Welpike <welpike@protonmail.com>
2023-09-01 11:15:20 +0200

    add license advice in rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,10 +7,20 @@ export default {
         {
             file: 'dist/cazan.js',
             format: 'es',
+            banner: "/*!\n" +
+                " * Cazan  v0.1.0 (https://github.com/cazanjs/cazan)\n" +
+                " * Copyright 2023 The Cazan Authors\n" +
+                " * Licensed under MIT (https://github.com/cazanjs/cazan/blob/main/LICENSE)\n" +
+                " */",
         },{
         file: 'dist/cazan.min.js',
             format: 'es',
             name: 'cazan',
+            banner: "/*!\n" +
+                " * Cazan  v0.1.0 (https://github.com/cazanjs/cazan)\n" +
+                " * Copyright 2023 The Cazan Authors\n" +
+                " * Licensed under MIT (https://github.com/cazanjs/cazan/blob/main/LICENSE)\n" +
+                " */",
             plugins: [terser()]
         }
     ],
