02f74ebe29960619e67bc98e201aa78e03caf067
Sellig6792 <sellig6792@gmail.com>
2023-09-04 20:56:02 +0200

    Fix test for image, make setup returns ctx and htmlelement of canvas and change the audio.mp3

--- tests/app.js
+++ tests/app.js
@@ -1,14 +1,15 @@
 import * as cazan from "/cazan.min.js"
 
-const game = document.querySelector( "#game" );
-
 function runApp() {
-    let ctx = cazan.setup("#game", "2d")
+    let [ctx, game] = cazan.setup("#game", "2d")
     let audio = new cazan.assets.Audio(["audio.mp3"])
     cazan.styles.setFill(ctx, "rgba(255,165,0,1)")
     let testRect = new cazan.shapes.Shape(ctx, {x: 10, y: 10}, {x: 50, y: 50})
     let testLine = new cazan.shapes.LineShape(ctx, {x: 10, y: 10}, {x: 50, y: 150})
-    let image = new cazan.assets.Image(ctx, {x: 10, y: 10}, {x: 200, y: 200}, "img.png")
+    let image = new cazan.assets.Image(ctx, "img.png", {x: 10, y: 10}, {x: 200, y: 200})
+
+    game.width = 800;
+    game.height = 450;
 
     // Pause for 3 seconds every 5 seconds
     setInterval(() => {
@@ -19,7 +20,6 @@ function runApp() {
     }, 5000);
 }
 
-game.width = 800;
-game.height = 450;
+
 
 window.addEventListener("load", runApp);
