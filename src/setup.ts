e1afdb5b86e7d3088103cddb6222640264aee1e4
Sellig6792 <sellig6792@gmail.com>
2023-09-07 21:42:06 +0200

    Add a Game class to help for configuration

--- src/setup.ts
+++ src/setup.ts
@@ -1,17 +1,18 @@
-import {ContextNotFoundException} from "./exceptions/index";
+import {Game} from "./game";
+import {ContextNotFoundException} from "./exceptions";
 import {CRenderingContext} from "./types/global";
 
 /**
  * Setup cazan
  * @param canvasSelector
  * @param context
- * @returns [CRenderingContext | null, HTMLCanvasElement]
+ * @returns {Game | ContextNotFoundException} -> Game or ContextNotFoundException
  * @throws ContextNotFoundException
  */
-export function setup(canvasSelector: string, context: string): ([CRenderingContext | null, HTMLCanvasElement]) | ContextNotFoundException {
+export function setup(canvasSelector: string, context: string): Game | ContextNotFoundException {
     let canvas: HTMLCanvasElement | null = document.querySelector(canvasSelector)
     if(canvas?.getContext) {
-        return [canvas.getContext(context) as CRenderingContext, canvas]
+        return new Game(canvas.getContext(context) as CRenderingContext, canvas)
     } else {
         return new ContextNotFoundException({filename: "cazan/setup.ts"})
     }
