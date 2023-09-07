fcee1b4714f3e99e723f68a6f1dc85d5537b2ff3
Welpike <welpike@protonmail.com>
2023-09-07 18:42:14 +0200

    add keyboard shortcuts handling

--- tests/app.js
+++ tests/app.js
@@ -12,6 +12,15 @@ function runApp() {
     let testLine = new cazan.shapes.LineShape(ctx, {x: 10, y: 10}, {x: 50, y: 150})
     let image = new cazan.assets.Image(ctx, "img.png", {x: 120, y: 10}, {x: 200, y: 200}, false)
 
+    cazan.events.keyboard.setShortcutHandler({
+        on: 'keydown',
+        shortcutCallback: (event) => (event.ctrlKey || event.metaKey) && event.key === 's',
+        callback: () => {
+            console.log('saved')
+            let ne = new cazan.shapes.Shape(ctx, {x: 160, y: 30}, {x: 45, y: 20})
+        }
+    })
+
     // Pause for 3 seconds every 5 seconds
     setInterval(() => {
         audio.play()
